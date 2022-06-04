4f1e844fe48b3112fc995e7f45556b5727fb81f3
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-06-04 19:16:53 -0300

    Added echo server main

--- SwiftQuicEcho/main.swift
+++ SwiftQuicEcho/main.swift
@@ -6,6 +6,48 @@
 //
 
 import Quic
+import Darwin
 
-print("Hello, World!")
+let certPEM = 2
+let keyPEM = 3
 
+let bootstrap: () async throws -> Server = {
+  async let bootServer = Server.bootstrap
+  // TODO: configure server
+  return try await bootServer()
+}
+
+let connect: (Server) async throws -> Connection = {
+  server in
+  try await server.accept()
+}
+
+let accept: (Connection) async throws -> Stream = {
+  connection in
+  try await connection.accept()
+}
+
+let receive: (Stream) async throws -> String = {
+  stream in
+  try await stream.receive()
+}
+
+let send: (Stream, String) async throws -> Void = {
+  stream, string in
+  try await stream.send(string)
+}
+
+let main = Task {
+  let server = try await bootstrap()
+  let connection = try await connect(server)
+  let stream = try await accept(connection)
+  for i in 0..<10 {
+    let string = try await receive(stream)
+    print(i, string)
+    try await send(stream, "Sending \(i)")
+  }
+}
+
+while true {
+  sleep(.max)
+}
