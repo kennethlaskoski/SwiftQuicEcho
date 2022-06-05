d675d251dd1a9acb73aae36e1d7fb91aedfa3b41
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-06-05 16:04:37 -0300

    Added multiplexing

--- SwiftQuicEcho/main.swift
+++ SwiftQuicEcho/main.swift
@@ -13,32 +13,24 @@ import Darwin
 let certPEM = 2
 let keyPEM = 3
 
-let bootstrap: () async throws -> Server = {
-  async let bootServer = Server.bootstrap
-  // TODO: configure server
-  return try await bootServer()
-}
-
-let connect: (Server) async throws -> Connection = {
-  server in
-  try await server.accept()
-}
-
-let accept: (Connection) async throws -> Quic.Stream = {
-  connection in
-  try await connection.accept()
-}
-
 let main = Task {
-  let server = try await bootstrap()
-  print("Started ...")
-  let connection = try await connect(server)
-  let stream = try await accept(connection)
-  print("Connected...")
-  while true {
-    let data = try await stream.receive()
-    print("Received \(data)")
-    try await stream.send(data)
+  let bootServer = Server.bootstrap
+  // TODO: configure server
+  let server = try await bootServer()
+  print("Server started ...")
+  try await withThrowingTaskGroup(of: Void.self) { group in
+    while true {
+      let connection = try await server.accept()
+      group.addTask {
+        let stream = try await connection.accept()
+        print("Connected...")
+        while true {
+          let data = try await stream.receive()
+          print("Received \(data)")
+          try await stream.send(data)
+        }
+      }
+    }
   }
 }
 
